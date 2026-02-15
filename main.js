2086da55287e9eb9d634d8adb04e8596db769047
Sathesh kumar <satheshs.sat@gmail.com>
2026-02-15 14:37:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+// Declare a route
+fastify.get('/', function handler (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
